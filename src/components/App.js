e79806a8358fc2c40ebebf9132a524aeee02c024
Daniel-Bichof <Daniel-Bichof@gmail.com>
2020-12-16 13:30:48 -0300

    iniciando

--- src/components/App.js
+++ src/components/App.js
@@ -8,8 +8,9 @@ export default class App extends React.Component{
         return(
             <div>
                 <Post title="Aprendendo reactJs com o Diogo da rocketSeat"/>
-                <Post title="Você pode passar uma propriedade do seu componente React"/>
-                <Post title="Com a propriedade do seu componente react vc coloca {this.props.propriedade}"/>
+                <Post title="O React funciona a base componentes, o componente é como uma Tag html em que você pode usar mexer no motorzinho debaixo do capô
+"/>
+                <Post title=" Na propriedade do seu componente react você pode usar {this.props.Nome-Da-propriedade} pra usar a propriedade do seu componente"/>
             </div>
         )
     }
